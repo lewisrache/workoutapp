750cf7bf466b7a5b5700f2baa5356e7ef71f842b
Rachel Lewis <rachellewis@rachels-mbp.lan>
2020-06-20 14:10:34 -0400

    tests

--- api_mark_ii/src/User.php
+++ api_mark_ii/src/User.php
@@ -3,12 +3,24 @@
 final class User
 {
     private $name;
+    private $programs;
     private function __construct($name)
     {
         $this->name = $name;
+        $this->programs = [];
     }
     public static function fromName(string $name): User
     {
         return new self($name);
+    }
+
+    public function addProgram(Program $program): void
+    {
+        $this->programs[] = $program;
+    }
+
+    public function getPrograms(): array
+    {
+        return $this->programs;
     }
 }
--- api_mark_ii/tests/UserTest.php
+++ api_mark_ii/tests/UserTest.php
@@ -10,4 +10,17 @@ final class UserTest extends TestCase
             User::fromName('newuser')
         );
     }
+
+    public function testAddProgramToUserAndGetItBack(): void
+    {
+        $exercises = [
+            Exercise::fromString("exercise1"),
+            Exercise::fromString("exercise2")
+        ];
+        $program = Program::create("programname", ...$exercises);
+        $user = User::fromName('newuser');
+        $user->addProgram($program);
+        $userPrograms = $user->getPrograms();
+        $this->assertEquals($program, $userPrograms[0]);
+    }
 }
