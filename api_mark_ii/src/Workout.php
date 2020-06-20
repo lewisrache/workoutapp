ef7668c8fcba445cf2149881249662b2f8a0b1ae
Rachel Lewis <rachellewis@rachels-mbp.lan>
2020-06-19 22:38:18 -0400

    tests

--- api_mark_ii/src/Workout.php
+++ api_mark_ii/src/Workout.php
@@ -3,9 +3,11 @@
 final class Workout
 {
     private $program;
+    private $components;
     private function __construct($program)
     {
         $this->program = $program;
+        $this->components = $this->cloneExercises(...$program->getExercises());
     }
     public static function fromProgram(Program $program): Workout
     {
@@ -14,5 +16,18 @@ final class Workout
     public function getName(): string
     {
         return $this->program->getName();
+    }
+    public function getComponents(): array
+    {
+        return $this->components;
+    }
+
+    private function cloneExercises(Exercise ...$exercises): array
+    {
+        $components = [];
+        foreach ($exercises as $exercise) {
+            $components[] = Component::fromExercise($exercise);
+        }
+        return $components;
     }
 }
--- api_mark_ii/tests/WorkoutTest.php
+++ api_mark_ii/tests/WorkoutTest.php
@@ -18,4 +18,26 @@ final class WorkoutTest extends TestCase
         $workout = Workout::fromProgram($program);
         $this->assertEquals($program->getName(), $workout->getName());
     }
+
+    public function testWorkoutGetsComponentClonesForProgramExercises(): void
+    {
+        $program = Program::create(
+            'programname',
+            ...[
+                Exercise::fromString('exercisename1'),
+                Exercise::fromString('exercisename2'),
+                Exercise::fromString('exercisename3'),
+            ]
+        );
+        $workout = Workout::fromProgram($program);
+        $programExercises = $program->getExercises();
+        $workoutComponents = $workout->getComponents();
+        // # components = # exercises
+        $this->assertEquals(count($programExercises), count($workoutComponents));
+        // NOTE: we don't _really_ care about the ordering, but it was the simplest way
+        // what we really care about is that foreach exercise there is a matching component (by name)
+        for ($i=0; $i<count($programExercises); $i++) {
+            $this->assertEquals($programExercises[$i]->getName(), $workoutComponents[$i]->getName());
+        }
+    }
 }
